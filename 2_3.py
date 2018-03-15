a77ebd8cf01c8c73d2e1ad6bd9a97e3911a6a7a7
Yebinfang <binfang.ye@nokia-sbell.com>
2018-03-15 18:27:28 +0800

    add 2.3 2.4

---
+++ 2_3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+name = 'Robin Ye'
+message = 'Hello' +' ' + name + ', '+ 'would you like to learn some Python today'
+print(message)
---
+++ 2_4_name_cases.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+name = 'Robin Ye'
+up_name = name.upper()
+lower_name = name.lower()
+title_name = name.title()
+print(up_name +'\n' + lower_name + '\n' + title_name)
+
