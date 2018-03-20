95654e6de8350d475b20b59309d84d2c06074529
Yebinfang <binfang.ye@nokia-sbell.com>
2018-03-20 19:56:01 +0800

    add 4.5-4.9

---
+++ chapter_4/4_10.py
@@ -0,0 +1,10 @@
+pizza = ['pizza A', 'pizza B', 'pizza C', 'pizza D']
+for food in pizza:
+    print("I like "+food)
+print("I really love pizza")
+print("The first three items in the list are:")
+print(pizza[0:3])
+print("Three items from the middle of the list are:")
+print(pizza[1:4])
+print("Three items from the middle of the list are:")
+print(pizza[1:4])
