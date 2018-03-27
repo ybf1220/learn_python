e30d53fa08da7da83dc71a3de042a0e6b499d735
Yebinfang <binfang.ye@nokia-sbell.com>
2018-03-27 10:15:02 +0800

    add 4.9 4.13

---
+++ chapter_4/4_13.py
@@ -0,0 +1,6 @@
+food = ('tomato', 'fish', 'orange', 'meat', 'banana')
+for f in food:
+    print(f)
+###error
+food[1] = 'chook'
+food = ('tomato', 'chook', 'orange', 'meat', 'dark')
--- chapter_4/4_9.py
+++ chapter_4/4_9.py
@@ -1,2 +1,3 @@
 lifang = [i ** 3 for i in range(1, 11)]
 print(lifang)
+, nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
